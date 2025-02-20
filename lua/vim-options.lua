2faa002425812cce379ffeb9bad49227e9a5cc68
josefbryja <499680@mail.muni.cz>
2025-02-20 22:36:06 +0100

    basic plugins

---
+++ lua/vim-options.lua
@@ -0,0 +1,9 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set relativenumber")
+vim.cmd("set number")
+--------------------------------------------------------------
+-- This allows to send highlighted code to the tmux pane 1
+vim.api.nvim_set_keymap('v', '<C-m>', ":w !tmux send-keys -t 1 \"$(cat)\" ENTER<CR>", { noremap = true, silent = true })
