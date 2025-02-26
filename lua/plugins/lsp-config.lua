61931ba8fc1f52cb4c91f09629d7847b047ca997
josefbryja <499680@mail.muni.cz>
2025-02-26 11:57:08 +0100

    adding lsp functionality, gitignore

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,6 +22,8 @@ return{
       lspconfig.texlab.setup({})
       lspconfig.marksman.setup({})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end
   }
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
 vim.cmd("set number")
+vim.g.mapleader = " "
 --------------------------------------------------------------
 -- This allows to send highlighted code to the tmux pane 1
 vim.api.nvim_set_keymap('v', '<C-m>', ":w !tmux send-keys -t 1 \"$(cat)\" ENTER<CR>", { noremap = true, silent = true })
