bcc6891a13e91b3a356a88a7c1147af78a6196de
JosefBryja <499680@mail.muni.cz>
2025-03-21 08:26:47 +0100

    slime

--- lua/plugins/tmux-navigator.lua
+++ lua/plugins/tmux-navigator.lua
@@ -1,9 +1,9 @@
-return { 
-  'alexghergh/nvim-tmux-navigation', 
+return {
+  'alexghergh/nvim-tmux-navigation',
   config = function()
     local nvim_tmux_nav = require('nvim-tmux-navigation')
     nvim_tmux_nav.setup {
-      disable_when_zoomed = true -- defaults to false
+      disable_when_zoomed = false -- defaults to false
     }
     vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
     vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
