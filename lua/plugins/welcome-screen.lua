002fce7afec5177ca7673250607b5b3408fbe71e
JosefBryja <499680@mail.muni.cz>
2025-03-23 22:53:41 +0100

    Welcome screen fix formating

--- lua/plugins/welcome-screen.lua
+++ lua/plugins/welcome-screen.lua
@@ -45,16 +45,16 @@ return {
       dashboard.section.footer.val = fortune {
         fortune_list = {
           { '“Not everything is a lesson, Ryan. Sometimes you just fail.”', '', '— Dwight K. Schrute III' },
-          {'“Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.”', '', '— Michael G. Scott'},
-          {' “If I had to, I could clean out my desk in five seconds, and nobody would ever know that I’d ever been here. And I’d forget, too.”', '', '— Ryan B. Howard'},
-          {'“Guess what, I have flaws. What are they? Oh, I don’t know. I sing in the shower. Sometimes I spend too much time volunteering. Occasionally I’ll hit somebody with my car. So sue me.”', '', '— Michael G. Scott'},
-          {'“I don’t hate it. I just don’t like it at all and it’s terrible.”', '', '— Michael G. Scott'},
-          {'“I think you’re under-thinking it."', '', '— Big Tuna'},
-          {'“I am running away from my responsibilities. And it feels good.”', '', '— Michael G. Scott'},
-          {'"I have a warm blanket and several cozy pillows. I read a chapter of a book and then it is lights out by 8:30. That is how I sleep at night."', '', '— Angela Martin'},
-          {'“And I knew exactly what to do. But in a much more real sense, I had no idea what to do.”', '', '— Michael G. Scott'},
-          {'“I talk a lot, so I’ve learned to tune myself out.”', '', '— Kelly R. Kapoor'},
-          {'“I guess I’ve been working so hard, I forgot what it’s like to be hardly working.”', '', '— Michael G. Scott'},
+          { '“Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.”', '', '— Michael G. Scott' },
+          { '“If I had to, I could clean out my desk in five seconds, and nobody would ever know that I’d ever been here. And I’d forget, too.”', '', '— Ryan B. Howard' },
+          { '“Guess what, I have flaws. What are they? Oh, I don’t know. I sing in the shower. Sometimes I spend too much time volunteering. Occasionally I’ll hit somebody with my car. So sue me.”', '', '— Michael G. Scott' },
+          { '“I don’t hate it. I just don’t like it at all and it’s terrible.”', '', '— Michael G. Scott' },
+          { '“I think you’re under-thinking it."', '', '— Big Tuna' },
+          { '“I am running away from my responsibilities. And it feels good.”', '', '— Michael G. Scott' },
+          { '"I have a warm blanket and several cozy pillows. I read a chapter of a book and then it is lights out by 8:30. That is how I sleep at night."', '', '— Angela Martin' },
+          { '“And I knew exactly what to do. But in a much more real sense, I had no idea what to do.”', '', '— Michael G. Scott' },
+          { '“I talk a lot, so I’ve learned to tune myself out.”', '', '— Kelly R. Kapoor' },
+          { '“I guess I’ve been working so hard, I forgot what it’s like to be hardly working.”', '', '— Michael G. Scott' },
         },
       }
 
