89020143cbb68fd553572fb5d2691b64dd7cd8ce
JosefBryja <499680@mail.muni.cz>
2025-03-30 11:13:16 +0200

    quarto

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,7 +23,7 @@ return{
       lspconfig.marksman.setup({})
       lspconfig.r_language_server.setup({})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end
   }
