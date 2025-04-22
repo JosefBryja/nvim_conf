0eb18eeabeadf61eb0364a6bf33c99b63ede0cff
JosefBryja <499680@mail.muni.cz>
2025-04-22 14:11:38 +0200

    spell check

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,6 +5,10 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
 vim.cmd("set number")
 vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>sp", function()
+  vim.opt.spell = not vim.opt.spell:get()
+  vim.opt.spelllang = "en_us"
+end, { desc = "Toggle spell check" })
 --------------------------------------------------------------
 -- This allows to send highlighted code to the tmux pane 2
 -- vim.api.nvim_set_keymap('v', '<C-m>', ":w !tmux send-keys -t 2 \"$(cat)\" ENTER<CR>", { noremap = true, silent = true })
