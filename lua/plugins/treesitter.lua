1fd3fedb9655a9e15fa57ea33f6c1199d0072613
JosefBryja <499680@mail.muni.cz>
2025-06-04 11:01:00 +0200

    treesitter - markdovn and markdown_inline

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "bash", "python", "r", "julia", "sql"},
+      ensure_installed = {"lua", "bash", "python", "r", "julia", "sql", "markdown", "markdown_inline"},
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false
